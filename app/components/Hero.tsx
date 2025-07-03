4e79d29aa146dfd9f0283f5c9b93741547130e07
unknown <arifk62901@gmail.com>
2025-07-03 07:46:41 +0600

    initial project done

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>Property type</h1>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+export default function Navbar() {
+  return <div>
+        <section>
+          <h1>RentYard</h1>
+        </section>
+        <section>
+          <button>Exit</button>
+        </section>
+
+        
+  </div>;
+}
